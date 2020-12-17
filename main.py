400d3a9b97105c7985a367a3857611638f77fcbc
Rosalie <rotre216@ulaval.ca>
2020-12-17 13:38:11 -0500

    changement de la boucle

--- main.py
+++ main.py
@@ -56,28 +56,31 @@ if __name__ == "__main__":
             succes = False
             while succes != True:
                 coup = partie.jouer_coup(1)
+                print(coup)
                 orientation = coup[0].split(' ')
 
                 if 'Placer' in orientation:
                     nouveau = api.jouer_coup(id_partie,'M'+'{}'.format(orientation[2][0].upper()), coup[1])
+                    print(nouveau)
                     nouveau['joueurs'][0]['pos'] = (nouveau['joueurs'][0]['pos'][0], nouveau['joueurs'][0]['pos'][1])
                     nouveau['joueurs'][1]['pos'] = (nouveau['joueurs'][1]['pos'][0], nouveau['joueurs'][1]['pos'][1])
                     partie.etat = nouveau
                     afficher_damier()
                 else:
                     nouveau = api.jouer_coup(id_partie, 'D', coup[1])
+                    print(nouveau)
                     nouveau['joueurs'][0]['pos'] = (nouveau['joueurs'][0]['pos'][0], nouveau['joueurs'][0]['pos'][1])
                     nouveau['joueurs'][1]['pos'] = (nouveau['joueurs'][1]['pos'][0], nouveau['joueurs'][1]['pos'][1])
                     partie.etat = nouveau
                     afficher_damier()
 
                 if partie.partie_terminée():
-                    win = True
                     print('Le gagnant est {}'.format(partie.partie_terminée()))
+                    succes = True
 
         else:
           succes = False
-          while not succes:
+          while succes != True:
               coup = input('Que voulez-vous faire?')
 
               if coup in  ('d', 'déplacement'):
